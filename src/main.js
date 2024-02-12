2ad50c17f7025f77a9a795e635d05bf9b32c1832
Gabriel <gabriel@webcgi.com.br>
2024-02-12 14:44:54 -0300

    Incluindo funcionalidades diversas dentro do formulário.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Maska from 'maska'
+import moment from 'moment'
+ 
 
-createApp(App).mount('#app')
+
+//createApp(App).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$moment = moment
+app.config.globalProperties.$moment.locale('pt-br')
+
+app.use(Maska)
+app.mount('#app')
